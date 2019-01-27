6f18b6a3df03a63646c208c8989107315b365890
Dhananjay <44304784+CaptainDTan@users.noreply.github.com>
2019-01-27 13:33:44 +0530

    Created Variables

---
+++ Variables.py
@@ -0,0 +1,40 @@
+#Variables
+
+''' Python features a dynamic type system and automatic memory management.
+So we can dynamically asign any value to a memory location just by naming it
+'''
+
+# example 
+
+name="something"
+
+'''this name is now of type string  
+you can find out its type by typing following command'''
+
+print(type(name))
+
+''' this statement will return following output
+
+<class 'str'>
+
+which means this is a string type variable
+'''
+
+#now lets change value of name
+
+name=10  #Remember there are no semicolons (;)😁👍👍
+
+'''
+now again printing the type of name 
+we will ge the following output
+'''
+
+print(type(name))
+
+'''output of above statement will be as below 
+
+<class 'int'>
+
+this means that there are no specific data type associated with variables 
+They are randomly generated and assigned in the memory
+'''
