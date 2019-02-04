215b817e8f550ef9d98ae56181f448961377e724
Dhananjay <44304784+CaptainDTan@users.noreply.github.com>
2019-02-04 19:28:10 +0530

    Created Conditions in Python

---
+++ Conditions.py
@@ -0,0 +1,27 @@
+# If condition in Python
+condition = True
+
+if condition == True :
+    print("The Condition is True")
+
+
+# If else Conditions in Python    
+Age = int(input("Enter Your Age"))
+
+if Age < 18 :
+    print("Sorrry \n You cant Drive")
+else :
+    print("Heyy You can Drive\n Get Your Lisence")
+    
+
+# If elif else Conditions in Python
+Age = int(input("Enter Your Age"))
+
+if Age < 5 :
+    print("Sorry You cant go to School")
+elif Age > 16 :
+    print("Go to Collage")
+else :
+    print("You are Welcome in School")
+
+#End
